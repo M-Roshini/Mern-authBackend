53be966c8bf8484cfd7d6234d3158aaab3afdfa5
Roshini <roshinim03@gmail.com>
2024-02-14 17:00:47 +0530

    live redis added

--- controllers/signin.js
+++ controllers/signin.js
@@ -21,7 +21,7 @@ async function InsertVerifyUser(name,email,password){
             password: hashedPassword,
             token:token,
         })
-        const activationLink =`http://localhost:4000/signin/${token}`
+        const activationLink =`https://mern-authbe.onrender.com/signin/${token}`
 
         const content=`<h4> hi, there</h4>
         <h5>Welcome to the app</h5>
